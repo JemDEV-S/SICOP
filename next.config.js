c6994b4e7a2d7bb0bc9e3cea59e19ad37661481a
JemDEV-S <jdryshq1234@gmail.com>
2026-05-19 11:28:00 -0500

    errores con el build 2

--- next.config.js
+++ next.config.js
@@ -2,8 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
   turbopack: {
     root: __dirname,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+    };
+
+    return config;
   },
 };
 
